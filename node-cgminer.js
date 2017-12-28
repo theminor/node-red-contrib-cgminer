4362160609edc7afef0ba49343db4dbdd4fc654f
John <theminor@users.noreply.github.com>
2017-12-28 14:07:41 -0500

    Modify parseMinerData() to retain other stats data

--- node-cgminer.js
+++ node-cgminer.js
@@ -21,19 +21,24 @@ module.exports = function(RED) {
 				return rtn;
 			};
 
-			var getMinerData = function(resultToParse) {		// return object containing the miner data obtained from cgminer
-				var minerDataArr = []
+			var parseMinerData = function(resultToParse) {		// return object containing the miner data obtained from cgminer
+				// var minerDataArr = []
 				for (var i = 0; i < resultToParse.STATS.length; i++) {										// cgminer api returns a count of miners under a given AUC3 controller for my Avalon Miners. Not sure if this is universal.
 					if (resultToParse.STATS[i].hasOwnProperty('MM Count')) {								// This contains data for each individual miner, and is really the main data I'm interested in for my setup...
-						for (var j = 1; j <= resultToParse.STATS[i]['MM Count']; j++) {						// MM Count starts with 1
-							minerDataArr.push(objectifyMinerData(resultToParse.STATS[i]['MM ID' + j]));
+						for (var j = 1; j <= resultToParse.STATS[i]['MM Count']; j++) {						// MM Count starts with 1, not 0
+							// minerDataArr.push(objectifyMinerData(resultToParse.STATS[i]['MM ID' + j]));
+							resultToParse.STATS[i]['MM ID' + j] = objectifyMinerData(resultToParse.STATS[i]['MM ID' + j]);		// replace MM ID data with object containing that data
 						}
 					}
 				}
-				return minerDataArr;
+				return resultToParse;
 			};
 
-			client.stats().then(function(cgMinerData) { msg.payload = getMinerData(cgMinerData); node.send(msg); }).done();		// parse data once promise is returned
+			// obtain stats from cgminer and send the data as msg payload
+			client.stats().then(function(cgMinerData) {
+				msg.payload = parseMinerData(cgMinerData);		// parse data once promise is returned
+				node.send(msg);									// msg.payload should contain object containing the miner data
+			}).done();
 		});
 	});
 };
