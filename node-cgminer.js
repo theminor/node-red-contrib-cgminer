5b6ca827a98e9255fc6b2ffcc1b0e4433aeee66b
John <theminor@users.noreply.github.com>
2018-01-29 15:35:55 -0500

    tidy code; remove debugging log messages

--- node-cgminer.js
+++ node-cgminer.js
@@ -4,15 +4,12 @@ var cgSendCmd = function (command, config, node, callback) {
 	var dataStg = '';
 	var socket;
 	try {
-		node.log('trying to connect to ' + config.ip + ':' + config.port);
 		socket = net.connect({ host: config.ip, port: config.port }, function () {
 			socket.on('data', function (res) { dataStg += res.toString(); });				// build data string as it is recieved
 			socket.on('end', function () {													// all data recieved from the response. Now pass to callback()
 				socket.removeAllListeners();
-				node.log('data recieved: ' + dataStg);
 				try { dataStg = JSON.parse(dataStg); }										// attempt to parse as an object, but if it fails, just return the string (my miners, for example, don't return proper json
 				catch(err) { node.warn('Error parsing json: ' + err); }
-				node.log('sending data: ' + dataStg);
 				callback(dataStg);
 				// return(dataStg);
 			});
@@ -34,13 +31,11 @@ module.exports = function(RED) {
 		RED.nodes.createNode(this, config);
 		var node = this;
 		node.on('input', function(msg) {													// input can be a simple string representing a command to send to cg miner, or an object (or json) for commands with
-			node.log('input: ' + msg.payload);
 			if (typeof msg.payload !== 'string') msg.payload = JSON.stringify(msg.payload);	// paremeters, like this: { command: command, parameter: parameter }
 			cgSendCmd(msg.payload, config, node, function(cgMinerData) {
 				msg.payload = cgMinerData;
 				msg.title = 'CGMiner Data';													// see https://github.com/node-red/node-red/wiki/Node-msg-Conventions
 				msg.description = 'Data from CGMiner';
-				node.log('msg.payload = ' + msg.payload);
 				node.send(msg);																// msg.payload should contain object containing the miner data (or a string, if parsing the object failed)
 			});
 		});
